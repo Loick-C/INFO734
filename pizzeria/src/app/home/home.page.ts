ee1803a02fe23b0960e73b46706eaca1314645bc
Little Kittenn <loick.combrie1@gmail.com>
2019-12-02 17:20:44 +0100

    back+front

--- pizzeria/src/app/home/home.page.ts
+++ pizzeria/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RestService } from '../rest.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  api : RestService;
+  platdujour : any;
+  constructor(public restapi : RestService, public loadingController : LoadingController) {
+    this.api=restapi;
+  }
 
-  constructor() {}
+  async getPDJ() {
+    const loading = await this.loadingController.create({
+      message: 'Loading'
+    });
+
+    await loading.present();
+    await this.api.getPDJ()
+      .subscribe(res => {
+        console.log(res[0]);
+        this.platdujour = res[0];
+        loading.dismiss();
+      }, err => {
+        console.log(err);
+        loading.dismiss();
+      });
+
+  }
+
+  ngOnInit() {
+    this.getPDJ();
+  }
 
 }
